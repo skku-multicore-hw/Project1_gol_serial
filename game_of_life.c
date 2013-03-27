f9061901ba4e0e570d2045a3e8f5d5727d166158
kbc8894 <kbc8894@gmail.com>
2013-03-27 19:36:07 +0900

    change commit game_of_life.c is changed

--- game_of_life.c
+++ game_of_life.c
@@ -6,32 +6,46 @@
 #include "game_of_life.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 
 // This function will return parameter and 3D_seed cell
 char*** Analysis_file(char* filename, int* parameters){
 
-	FILE *pFile_in;
-	char ch;
-	char buf[10];
-	int par=0,n=0;
+	int nFile_in;
+	int len;
+	char temp[10];
+	char *buf;
+	int par=0,n=0,m=0;
 
-	if((pFile_in = fopen("input.life","r")) == NULL){
+	char ***Return_Cube;
+
+	if((nFile_in = open(filename,O_RDONLY)) < 0){
 		printf("file open error!\n");
 		exit(1);
 	}
 
+	len = lseek(nFile_in,0,SEEK_END);
+	buf = (char *)malloc(sizeof(char)*len);
+	lseek(nFile_in,0,SEEK_SET);
+	read(nFile_in,buf,len);
+
 	while(1){
-		ch=fgetc(pFile_in);
-		if(ch == '\n'){
-			parameters[par] = atoi(buf);
+		if(buf[n] == '\n'){
+			parameters[par++] = atoi(temp);
+			n++;
 			break;
 		}
-		else if(ch == ' '){
-			parameters[par++] = atoi(buf);
-			n = 0;
+		else if(buf[n] == ' '){
+			parameters[par++] = atoi(temp);
+			m = 0;
 		}
-		else buf[n++] = ch;
+		else  temp[m++]= buf[n];
+		n++;
 	}
+
+	printf("%d\n",n);
 
 	return NULL;
 }
@@ -41,7 +55,7 @@ void game_of_life(char* filename){
 
 	int parameters[Parameter];
 	char ***Cube;
-	int i;
+//	int i;
 
 	Cube = Analysis_file(filename,parameters);
 
