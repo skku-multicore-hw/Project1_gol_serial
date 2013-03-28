acb695b007b29df5f5d65e096d41545e3039fdb9
minkyun <minkyun@ubuntu>
2013-03-28 19:54:43 +0900

    sumit process.c process.h

---
+++ process.h
@@ -0,0 +1,9 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+char**** process_main(int* parameters, char**** cube);
+char odd;				//use for vector select e) vector[odd][i][j][k]
+
+#endif
